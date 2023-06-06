f21bad0253b0e3871f25e66918041266fe03fbde
kurumadaisuke <kuruma0909@gmail.com>
2023-06-06 17:35:50 +0900

    ユーザーにカラムを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'books#index'
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   resources :books
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
