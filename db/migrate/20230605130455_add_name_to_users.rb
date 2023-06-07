668255af7cf7d875b5965ea96aed4e821d660b0d
kurumadaisuke <kuruma0909@gmail.com>
2023-06-08 06:36:38 +0900

    履歴残し

---
+++ db/migrate/20230605130455_add_name_to_users.rb
@@ -0,0 +1,8 @@
+class AddNameToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :address, :string
+    add_column :users, :postal_code, :integer
+    add_column :users, :profile, :text
+  end
+end
