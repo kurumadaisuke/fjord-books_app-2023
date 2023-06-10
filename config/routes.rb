3d2ecf8796d3c8a9d164e86fe35fd5b875eb5432
kurumadaisuke <kuruma0909@gmail.com>
2023-06-10 14:23:14 +0900

    履歴残し

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
-  # devise_for :users, controllers: { registrations: 'users/registrations' }
-  #   get "users/show" => "users#show"
-  # get 'users/show'
-
   root to: 'books#index'
+  resources :books
   devise_for :users, controllers: { registrations: 'users/registrations' }
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
     get '/users/index' => 'users#index'
     get '/users/:id' => 'users#show'
+    get '/users/sign_up' => 'users#new'
+    get '/users/edit' => 'users#edit'
   end
-  resources :books
 end
