d3dda7723b456c502b191636b469b8e82096c6af
kurumadaisuke <kuruma0909@gmail.com>
2023-06-12 22:02:25 +0900

    履歴残し

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
     get '/users/sign_up' => 'users#new'
     get '/users/edit' => 'users#edit'
   end
+  
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
