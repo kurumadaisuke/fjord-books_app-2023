5a86d734f3795709d5244be4bf5268fbe16ff83e
kurumadaisuke <kuruma0909@gmail.com>
2023-06-29 10:17:36 +0900

    修正点を修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,6 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by(id: params[:id])
+    @user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
   root to: 'books#index'
   resources :books
-  devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+  }
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
-    get '/users/index' => 'users#index'
-    get '/users/:id' => 'users#show'
-    get '/users/sign_up' => 'users#new'
-    get '/users/edit' => 'users#edit'
+    resources :users
   end
 
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
