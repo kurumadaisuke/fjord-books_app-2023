4109158f86b35ab8c0e710350309f345aa99c6cc
kurumadaisuke <kuruma0909@gmail.com>
2023-07-02 07:47:37 +0900

    Book,Reportに日報機能の削除機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,11 @@ Rails.application.routes.draw do
   resources :users, only: %i(index show)
 
   resources :books do
-    resources :comments, only: :create, module: :books
+    resources :comments, only: [:create], module: :books
   end
+  resources :comments, only: [:destroy]
 
   resources :reports do
-    resources :comments, only: :create, module: :reports
+    resources :comments, only: [:create, :destroy], module: :reports
   end
 end
