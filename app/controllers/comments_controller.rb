67e90fcfbfe04c715f7d14159a46f8642be57430
kurumadaisuke <kuruma0909@gmail.com>
2023-07-02 14:41:44 +0900

    必要なファイルコミット

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class CommentsController < ApplicationController
+  def create
+    @comment = @commentable.comments.build(comment_params)
+    @comment.user = current_user
+    if @comment.save
+      redirect_to @commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human)
+    else
+      @comments = @commentable.comments
+      render_commentable_show
+    end
+  end
+
+  def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy
+    flash[:notice] = 'コメントを削除しました'
+    redirect_to book_path
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+end
