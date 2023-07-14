3b1f6bff33737abd1a273d6f7c691c77e02a3315
kurumadaisuke <kuruma0909@gmail.com>
2023-07-14 15:37:35 +0900

    ログ残し。

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,10 +3,10 @@
 class CommentsController < ApplicationController
   def destroy
     comment = Comment.find(params[:id])
+    type_path = comment.commentable_type.downcase
     return if comment.user != current_user
-
     comment.destroy
     flash[:notice] = 'コメントを削除しました'
-    redirect_to request.referer
+    redirect_to comment_path(id: comment.commentable_id)
   end
 end
