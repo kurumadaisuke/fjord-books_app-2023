3b3824c312c116ea74353968eaed5c54026e5c92
kurumadaisuke <kuruma0909@gmail.com>
2023-08-10 22:22:44 +0900

    reverse_of_relationships名変更

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,10 +3,11 @@
 class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
+
   has_many :report_mention, dependent: :destroy
   has_many :mentioning_reports, through: :report_mention, source: :mention
-  has_many :reverse_of_relationships, class_name: 'ReportMention', foreign_key: 'mention_id', dependent: :destroy, inverse_of: :mention
-  has_many :mentioned_reports, through: :reverse_of_relationships, source: :report
+  has_many :mentioned_reports_relationships, class_name: 'ReportMention', foreign_key: 'mention_id', dependent: :destroy, inverse_of: :mention
+  has_many :mentioned_reports, through: :mentioned_reports_relationships, source: :report
 
   validates :title, presence: true
   validates :content, presence: true
