599da0b01906e5b0064ea489325e10a4297f01c9
kurumadaisuke <kuruma0909@gmail.com>
2023-08-17 13:09:08 +0900

    ログ残し（Controllerアンチパターン）

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -20,22 +20,28 @@ class ReportsController < ApplicationController
   def edit; end
 
   def create
-    @report = current_user.reports.new(report_params)
+    ActiveRecord::Base.transaction do
+      @report = current_user.reports.new(report_params)
 
-    if @report.save
-      mention_reports_create
-      redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human)
-    else
-      render :new, status: :unprocessable_entity
+      if @report.save!
+        mention_reports_create
+        redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human)
+      else
+        render :new, status: :unprocessable_entity
+        raise ActiveRecord::Rollback
+      end
     end
   end
 
   def update
-    if @report.update(report_params)
-      mention_reports_update
-      redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
-    else
-      render :edit, status: :unprocessable_entity
+    ActiveRecord::Base.transaction do
+      if @report.update!(report_params)
+        mention_reports_update
+        redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
+      else
+        render :edit, status: :unprocessable_entity
+        raise ActiveRecord::Rollback
+      end
     end
   end
 
